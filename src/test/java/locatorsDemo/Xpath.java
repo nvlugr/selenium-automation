82672aa4ec8f5050ea882e2cc856c0f5b2a522d9
murtazaaylak <nevalugr@gmail.com>
2023-05-15 22:10:02 +0300

    docs: adds Xpath file to the package

---
+++ src/test/java/locatorsDemo/Xpath.java
@@ -0,0 +1,31 @@
+package locatorsDemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Xpath {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.opencart.com/");
+		driver.manage().window().maximize();
+		
+		// //tag[attribute='value']
+		
+		//Relative Xpath
+		driver.findElement(By.xpath("//a[normalize-space()='Resources']")).click();
+		
+		//Absolute Xpath
+		driver.findElement(By.xpath("/html[1]/body[1]/header[1]/nav[1]/div[1]/div[2]/ul[1]/li[6]/ul[1]/li[1]/a[1]")).click();
+		
+		//Xpath Methods
+		//text() for static texts
+		driver.findElement(By.xpath("//a[text()='Visit Website']")).click();	
+		// contain(text(), 'text') for dynamic texts
+		driver.findElement(By.xpath("//a [contains(text(), 'Tools')]")).click();
+		
+		
+	}
+	
+}
