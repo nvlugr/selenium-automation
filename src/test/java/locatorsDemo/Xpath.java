4107389481301a11f9d420dc9da8a7767f3d916a
murtazaaylak <nevalugr@gmail.com>
2023-05-16 21:19:53 +0300

    feat: adds Xpath axes

--- src/test/java/locatorsDemo/Xpath.java
+++ src/test/java/locatorsDemo/Xpath.java
@@ -20,12 +20,26 @@ public class Xpath {
 		driver.findElement(By.xpath("/html[1]/body[1]/header[1]/nav[1]/div[1]/div[2]/ul[1]/li[6]/ul[1]/li[1]/a[1]")).click();
 		
 		//Xpath Methods
+		
 		//text() for static texts
-		driver.findElement(By.xpath("//a[text()='Visit Website']")).click();	
-		// contain(text(), 'text') for dynamic texts
+		
+		driver.findElement(By.xpath("//a[text()='Visit Website']")).click();
+		
+		// contain(text(), 'text') for dynamic texts-partial text
+		
 		driver.findElement(By.xpath("//a [contains(text(), 'Tools')]")).click();
+		
+		//start-with(text(),'Pragmatic')
+		
+		//More than one attribute: tagName[@attribute='value'][@attribute='value']
+		//[@attribute='value' and @attribute='value']
+		
+		
+		
+		
+		
 		
 		
 	}
 	
-}
+}    
